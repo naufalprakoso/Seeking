1c8606804526d7ac6001fb32c72a0092ac586d0f
naufalprakoso <naufalprakoso24@gmail.com>
2018-05-13 08:07:26 +0700

    add splash screen

---
+++ app/src/main/java/com/njy/seeking/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.njy.seeking;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int splashInterval = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), UserHomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, splashInterval);
+    }
+}
